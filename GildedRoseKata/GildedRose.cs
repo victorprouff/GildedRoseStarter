7107d7da342cde8602f686f1e794b7ec83436fed
Victor Prouff <contact@victorprouff.fr>
2023-08-12 16:50:46 +0200

    Remove methods

--- GildedRoseKata/GildedRose.cs
+++ GildedRoseKata/GildedRose.cs
@@ -30,7 +30,21 @@ public class GildedRose
                 {
                     Items[i].Quality = Items[i].Quality + 1;
 
-                    CalculateQualityBySellInForBackstagePasses(Items[i]);
+                    if (Items[i].SellIn < 11)
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                    }
+
+                    if (Items[i].SellIn < 6)
+                    {
+                        if (Items[i].Quality < 50)
+                        {
+                            Items[i].Quality = Items[i].Quality + 1;
+                        }
+                    }
                 }
 
                 Items[i].SellIn = Items[i].SellIn - 1;
@@ -49,7 +63,10 @@ public class GildedRose
             {
                 if (Items[i].Name == "Aged Brie")
                 {
-                    Add1QualityIfQualityLowerTo50(Items[i]);
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+                    }
                 }
                 else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
@@ -66,27 +83,6 @@ public class GildedRose
                     }
                 }
             }
-        }
-    }
-
-    private void CalculateQualityBySellInForBackstagePasses(Item item)
-    {
-        if (item.SellIn < 11)
-        {
-            Add1QualityIfQualityLowerTo50(item);
-        }
-
-        if (item.SellIn < 6)
-        {
-            Add1QualityIfQualityLowerTo50(item);
-        }
-    }
-
-    private void Add1QualityIfQualityLowerTo50(Item item)
-    {
-        if (item.Quality < 50)
-        {
-            item.Quality = item.Quality + 1;
         }
     }
 }
