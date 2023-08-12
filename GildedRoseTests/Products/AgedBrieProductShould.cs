8ae93e05d94f4d0d6e5eeb4fc2756ab8c2e1b668
Victor Prouff <contact@victorprouff.fr>
2023-08-12 19:45:09 +0200

    Add Unit tests AgedBrieProduct

---
+++ GildedRoseTests/Products/AgedBrieProductShould.cs
@@ -0,0 +1,24 @@
+using FluentAssertions;
+
+namespace GildedRoseKata.Products;
+
+public class AgedBrieProductShould
+{
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 3)]
+    [InlineData(4, 5)]
+    [InlineData(5, 7)]
+    public void QualityDeclines(int numberOfTurns, int expectedQuality)
+    {
+        var product = new AgedBrieProduct(3);
+
+        for (var turn = 0; turn < numberOfTurns; turn++)
+        {
+            product.UpdateQuality();
+        }
+
+        product.Quality.Should().Be(expectedQuality);
+    }
+}
