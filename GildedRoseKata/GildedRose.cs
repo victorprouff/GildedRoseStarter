fb6861b202e8eab804fc4d1c93042ae0aa5ee4c7
Victor Prouff <contact@victorprouff.fr>
2023-08-12 16:39:15 +0200

    Split if

--- GildedRoseKata/GildedRose.cs
+++ GildedRoseKata/GildedRose.cs
@@ -14,16 +14,21 @@ public class GildedRose
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            if (Items[i].Name == "Aged Brie" || Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+            if (Items[i].Name == "Aged Brie")
+            {
+                if (Items[i].Quality < 50)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
+                }
+            }
+            else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
             {
                 if (Items[i].Quality < 50)
                 {
                     Items[i].Quality = Items[i].Quality + 1;
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        CalculateBackstagePasses(Items[i]);
-                    }
+                    CalculateBackstagePasses(Items[i]);
+
                 }
             }
             else
