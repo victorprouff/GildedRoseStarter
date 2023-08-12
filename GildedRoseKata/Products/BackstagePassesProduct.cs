18ae062594b6488871e9cf839dcc3af0b6249646
Victor Prouff <contact@victorprouff.fr>
2023-08-12 18:03:36 +0200

    Invert if in UpdateQuality BackstagePassesProduct method

--- GildedRoseKata/Products/BackstagePassesProduct.cs
+++ GildedRoseKata/Products/BackstagePassesProduct.cs
@@ -4,24 +4,23 @@ public class BackstagePassesProduct : Product
 {
     public override void UpdateQuality()
     {
-        if (Quality < 50)
+        if (Quality >= 50) return;
+
+        Quality++;
+
+        if (SellIn < 11)
         {
-            Quality = Quality + 1;
-
-            if (SellIn < 11)
+            if (Quality < 50)
             {
-                if (Quality < 50)
-                {
-                    Quality = Quality + 1;
-                }
+                Quality++;
             }
+        }
 
-            if (SellIn < 6)
+        if (SellIn < 6)
+        {
+            if (Quality < 50)
             {
-                if (Quality < 50)
-                {
-                    Quality = Quality + 1;
-                }
+                Quality++;
             }
         }
     }
